1df10660eb4a5b2666f50e6f308077b0247a0e84
Guomao Zhang <zgmmail@gmail.com>
2014-12-07 22:48:57 +0800

    add new

--- leetcode.cpp
+++ leetcode.cpp
@@ -18,6 +18,22 @@ public:
         }
         return left;
     }
+
+    
+    double findMedian(int A[], int m, int B[], int n, int k){
+        if(m>n) return findMedian(B,n,A,m,k);
+        if(m==0) return B[k-1];
+        if(k==1) return min(A[0],B[0]);
+        
+        int md = min(m,k/2), nd = k-md;
+        if(A[md-1]<B[nd-1]) return findMedian(A+md,m-md,B,nd,k-md);
+        else return findMedian(A,md,B+nd,n-nd,md);
+    }
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        int mn = m+n;
+        if(mn&1) return findMedian(A,m,B,n,mn/2+1);
+        else return (findMedian(A,m,B,n,mn/2)+findMedian(A,m,B,n,mn/2+1))/2;
+    }
 };
 
 
